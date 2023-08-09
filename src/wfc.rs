3a6d311ae50fc74dd8b1e97c966f30c635377457
Ria8651 <bk.dev@icloud.com>
2023-08-09 16:45:44 +1200

    merge fair lowest entropy

--- src/wfc.rs
+++ src/wfc.rs
@@ -51,14 +51,29 @@ impl GraphWfc {
     }
 
     pub fn lowest_entropy(graph: &mut Graph<Cell>) -> Option<usize> {
+        let mut rng = rand::thread_rng();
+
         // find next cell to update
         let mut min_entropy = usize::MAX;
         let mut min_index = None;
+        let mut with_min: usize = 0; // Track how many nodes has the lowest entropy found
         for (index, node) in graph.tiles.iter().enumerate() {
             let entropy = node.count_bits();
-            if entropy > 1 && entropy < min_entropy {
-                min_entropy = entropy;
-                min_index = Some(index);
+            if entropy > 1 && entropy <= min_entropy {
+                with_min += 1;
+                if entropy < min_entropy {
+                    with_min = 1;
+                    min_entropy = entropy;
+                    min_index = Some(index);
+                } else {
+                    with_min += 1;
+
+                    // Select new node so that all nodes with min_entropy have equal chance of been chosen
+                    if rng.gen_bool(1.0f64 / with_min as f64) {
+                        min_entropy = entropy;
+                        min_index = Some(index);
+                    }
+                }
             }
         }
 
