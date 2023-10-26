45d1aa0c0af46bc2f4e821182e92b8be5683fec7
Robert Christie <r.b.christie@icloud.com>
2023-10-26 14:58:58 +1300

    failure rate results

--- benches/failure_rate.rs
+++ benches/failure_rate.rs
@@ -1,4 +1,8 @@
-use core_wfc::{wfc_backend::MultiThreaded, wfc_task::WfcSettings, TileSet};
+use core_wfc::{
+    wfc_backend::{Backend, MultiThreaded},
+    wfc_task::WfcSettings,
+    TileSet, WaveFunction, WfcTask,
+};
 use grid_wfc::{
     grid_graph::GridGraphSettings,
     mxgmn_tileset::MxgmnTileset,
@@ -33,59 +37,99 @@ fn main() {
     }
 
     for generation_type in [
-        ChunkingTests::Interior,
-        ChunkingTests::Exterior,
-        ChunkingTests::Ours,
-        ChunkingTests::OursDiscard,
-        ChunkingTests::OursDeterministic,
+        None,
+        Some(ChunkingTests::Interior),
+        Some(ChunkingTests::Exterior),
+        Some(ChunkingTests::Ours),
+        Some(ChunkingTests::OursDiscard),
+        Some(ChunkingTests::OursDeterministic),
     ]
     .into_iter()
     {
         let mut failures = 0;
-        for _ in 0..ITTERATIONS {
-            let settings = GridGraphSettings {
-                height: SIZE,
-                width: SIZE,
-                periodic: false,
-            };
 
-            let merging = match generation_type {
-                ChunkingTests::Interior => ChunkMerging::Interior,
-                ChunkingTests::Exterior => ChunkMerging::Full,
-                ChunkingTests::Ours
-                | ChunkingTests::OursDiscard
-                | ChunkingTests::OursDeterministic => ChunkMerging::Mixed,
-            };
-            let discard = match generation_type {
-                ChunkingTests::OursDiscard | ChunkingTests::OursDeterministic => DISCARD,
-                _ => 0,
-            };
-            let generation_mode = match generation_type {
-                ChunkingTests::OursDeterministic => GenerationMode::Deterministic,
-                _ => GenerationMode::NonDeterministic,
-            };
+        if let Some(method) = generation_type {
+            for i in 0..ITTERATIONS {
+                dbg!(i);
+                let settings = GridGraphSettings {
+                    height: SIZE,
+                    width: SIZE,
+                    periodic: false,
+                };
 
-            let (_, err) = single_shot::generate_world(
-                tileset.clone(),
-                &mut backend,
-                settings,
-                seed,
-                generation_mode,
-                ChunkSettings {
-                    merging,
-                    size: CHUNK_SIZE,
-                    overlap: OVERLAP,
-                    discard,
-                    ..Default::default()
-                },
-                WfcSettings {
-                    ..Default::default()
-                },
-            );
+                let merging = match method {
+                    ChunkingTests::Interior => ChunkMerging::Interior,
+                    ChunkingTests::Exterior => ChunkMerging::Full,
+                    ChunkingTests::Ours
+                    | ChunkingTests::OursDiscard
+                    | ChunkingTests::OursDeterministic => ChunkMerging::Mixed,
+                };
+                let discard = match method {
+                    ChunkingTests::OursDiscard | ChunkingTests::OursDeterministic => DISCARD,
+                    _ => 0,
+                };
+                let generation_mode = match method {
+                    ChunkingTests::OursDeterministic => GenerationMode::Deterministic,
+                    _ => GenerationMode::NonDeterministic,
+                };
 
-            failures += err.is_err() as usize;
-            seed += 1;
+                let (_, err) = single_shot::generate_world(
+                    tileset.clone(),
+                    &mut backend,
+                    settings,
+                    seed,
+                    generation_mode,
+                    ChunkSettings {
+                        merging,
+                        size: CHUNK_SIZE,
+                        overlap: OVERLAP,
+                        discard,
+                        ..Default::default()
+                    },
+                    WfcSettings {
+                        ..Default::default()
+                    },
+                );
+
+                failures += err.is_err() as usize;
+                seed += 1;
+            }
+        } else {
+            for _ in 0..ITTERATIONS {
+                let settings = GridGraphSettings {
+                    height: SIZE,
+                    width: SIZE,
+                    periodic: false,
+                };
+
+                let backend: &mut dyn Backend = &mut backend;
+
+                let graph = grid_wfc::grid_graph::create(
+                    &settings,
+                    WaveFunction::filled(tileset.tile_count()),
+                );
+
+                backend
+                    .queue_task(WfcTask {
+                        update_channel: None,
+                        graph,
+                        seed,
+                        tileset: tileset.clone(),
+                        metadata: None,
+                        settings: WfcSettings {
+                            ..Default::default()
+                        },
+                    })
+                    .unwrap();
+            }
+            for i in 0..ITTERATIONS {
+                dbg!(i);
+                let (_, err) = backend.wait_for_output();
+                failures += err.is_err() as usize;
+                seed += 1;
+            }
         }
+
         println!(
             "{:?} failures: {} / {}",
             generation_type, failures, ITTERATIONS
